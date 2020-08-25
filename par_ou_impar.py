a88d5d2c19ba7c04a34a39ddc52d41b253ee131e
edmilsonchaves <58867496+edmilsonchaves@users.noreply.github.com>
2020-08-25 20:26:01 -0300

    Add files via upload

---
+++ par_ou_impar.py
@@ -0,0 +1,13 @@
+"""
+Faça um programa que mostre se o valor informado é par ou ímpar.
+
+"""
+
+number = int(input("Digite o valor: "))
+
+if number % 2 == 0:
+    print(f'O número {number} é par!')
+
+else:
+    print(f'O número {number} é ímpar!')
+
